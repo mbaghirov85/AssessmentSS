d1169fb68a1e82da928d7b50875a25c08f572df1
Murad Baghirov <mbaghirov85@gmail.com>
2025-01-01 09:38:49 +0000

    feat(refactoring): bringed models to the SOLID principles, prettified few other files for better visibility

---
+++ assessment-api-developer/Models/CustomerDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace assessment_platform_developer.Models
+{
+    public class CustomerDBContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
